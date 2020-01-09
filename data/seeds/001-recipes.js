c5eb2eeac1dd80b57bb901906f3154af455a8bfa
spettigrew <sara.pettigrew5@gmail.com>
2020-01-09 15:28:25 -0700

    Recipes added

--- data/seeds/001-recipes.js
+++ data/seeds/001-recipes.js
@@ -1,8 +1,9 @@
 
 exports.seed = async (knex) => {
-  await knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-};
+  await knex("recipes").insert([
+        { id: 1, name: "Easy Chocolate Fudge Cake" },
+        { id: 2, name: "Chocolate Chip Tea Bread" },
+        { id: 3, name: "Chocolate & Raspberry Souffles" },
+        { id: 4, name: "Chocolate Puddle Cake" }
+      ])
+}
