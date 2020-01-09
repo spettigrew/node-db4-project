13ce72061472a41731d3b3fcc0691280082a7caf
spettigrew <sara.pettigrew5@gmail.com>
2020-01-08 22:02:03 -0700

    Server file setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const recipeRouter = require('./recipes/recipe-router');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/recipes', recipeRouter);
+
+server.get("/", (req, res, next) => {
+    res.send("<h2>I am your sanity check.</h2>")
+})
+
+module.exports = server;
