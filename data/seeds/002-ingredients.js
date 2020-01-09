8f6612eba448de3ee7858d10b0d091eacc4e0735
spettigrew <sara.pettigrew5@gmail.com>
2020-01-09 15:30:15 -0700

    Adding Ingredients

--- data/seeds/002-ingredients.js
+++ data/seeds/002-ingredients.js
@@ -1,8 +1,22 @@
 
 exports.seed = async (knex) => {
-  await knex('table_name').insert([
-    { id: 1, colName: 'rowValue1' },
-    { id: 2, colName: 'rowValue2' },
-    { id: 3, colName: 'rowValue3' }
-  ]);
-};
+  await knex("ingredients").insert([
+    { id: 1, name: "Semisweet Chocolate" },
+    { id: 2, name: "Eggs" },
+    { id: 3, name: "Salted Butter" },
+    { id: 4, name: "Light Cream" },
+    { id: 5, name: "Unsweetened Cocoa Powder" },
+    { id: 6, name: "Milk Chocolate" },
+    { id: 7, name: "Raspberries" },
+    { id: 8, name: "Unsalted Butter" },
+    { id: 9, name: "White Sugar" },
+    { id: 10, name: "Flour" },
+    { id: 11, name: "Baking Powder" },
+    { id: 12, name: "Light Brown Sugar" },
+    { id: 13, name: "Powdered Sugar" },
+    { id: 14, name: "Eggs" },
+    { id: 15, name: "Eggs" },
+    { id: 16, name: "Eggs" },
+    { id: 17, name: "Eggs" },
+  ])
+}
