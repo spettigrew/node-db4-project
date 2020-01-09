d13d1dd9f52ba12e6a9c78cdb9ef45897033053e
spettigrew <sara.pettigrew5@gmail.com>
2020-01-08 22:15:09 -0700

    Recipe model setup

---
+++ recipes/recipe-model.js
@@ -0,0 +1,40 @@
+const db = require("../db-config")
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db("recipes")
+}
+
+function findById(id) {
+    return db("recipes")
+    .where({ id })
+    .first()
+}
+
+async function add(recipeName) {
+    const [id] = await db("recipes")
+    .insert(recipeName)
+    return db("recipes")
+    .where({ id })
+    .first()
+}
+
+function update(ingredients, id) {
+    return db("recipes")
+    .where({ id })
+    .update(ingredients)
+}
+
+function remove(id) {
+    return db("recipes")
+    .where({ id })
+    .del()
+}
+
