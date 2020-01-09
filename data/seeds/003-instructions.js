4ba1c91e230c2a1c270c0885f885709a63ea5e0a
spettigrew <sara.pettigrew5@gmail.com>
2020-01-09 15:31:45 -0700

    Seed setup

--- data/seeds/003-instructions.js
+++ data/seeds/003-instructions.js
@@ -1,8 +1,8 @@
 
 exports.seed = async (knex) => {
   await knex('table_name').insert([
-    { id: 1, colName: 'rowValue1' },
-    { id: 2, colName: 'rowValue2' },
-    { id: 3, colName: 'rowValue3' }
+    { id: 1, name: 'rowValue1' },
+    { id: 2, name: 'rowValue2' },
+    { id: 3, name: 'rowValue3' }
   ]);
 };
