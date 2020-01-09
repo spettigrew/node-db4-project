7d719acc3f3c99e8b97c2b3826015cb838f3906f
spettigrew <sara.pettigrew5@gmail.com>
2020-01-08 22:14:49 -0700

    Router api endpoint calls setup

---
+++ recipes/recipe-router.js
@@ -0,0 +1,59 @@
+const express = require("express")
+
+const Recipes = require("./recipe-model")
+
+const router =  express.Router()
+
+router.get("/", async (req, res, next) => {
+    try {
+        await
+    } 
+    catch(err) {
+        next(err)
+    }
+})
+
+router.get("/", async (req, res, next) => {
+    try {
+        await
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.get("/", async (req, res, next) => {
+    try {
+        await
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        await
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.put("/", async (req, res, next) => {
+    try {
+        await
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.del("/", async (req, res, next) => {
+    try {
+        await
+    }
+    catch (err) {
+        next(err)
+    }
+})
