e6f61706d6c95d8c749a33fb207743900c0ce2f5
spettigrew <sara.pettigrew5@gmail.com>
2020-01-10 13:15:52 -0700

    Instructions migration complete

--- data/migrations/20200109160936_instructions.js
+++ data/migrations/20200109160936_instructions.js
@@ -3,26 +3,16 @@ exports.up = async function(knex) {
     await knex.schema.createTable("instructions", (table) => {
         table.increments("id")
         table.string("name").notNullable()
-        table.integer("step").notNullable()
-        table.string("number").notNullable()
+        table.integer("steps").notNullable()
+        table.string("step_number").notNullable()
         // OR
         // table.string("one").notNullable()
         //table.string("two").notNullable()
         //table.string("three").notNullable()
         //table.string("four").notNullable()
     })
-
-    await knex.schema.createTable("shopping_list", (table) => {
-        table.integer("instructions_id")
-            .notNullable()
-            .references("id")
-            .inTable("instructions")
-            .onDelete("CASCADE")
-            .onUpdate("CASCADE")
-    })
 };
 
-exports.down = function(knex) {
-    await knex.schema.dropTableIfExists("shopping_list")
+exports.down = async function(knex) {
     await knex.schema.dropTableIfExists("instructions")
 };
