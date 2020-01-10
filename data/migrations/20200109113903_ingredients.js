2becb185d3a99e9b1dfc0f13dbe368895fbbeb89
spettigrew <sara.pettigrew5@gmail.com>
2020-01-09 20:00:30 -0700

    Ingredient migration setup

--- data/migrations/20200109113903_ingredients.js
+++ data/migrations/20200109113903_ingredients.js
@@ -1,8 +1,21 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable("ingredients", (table) => {
+        table.increments("id")
+        table.string("name").notNullable()
+    })
+
+    await knex.schema.createTable("shopping_list", (table) => {
+        table.integer("ingredients_id")
+            .notNullable()
+            .references("id")
+            .inTable("ingredients")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+    })
 };
 
 exports.down = function(knex) {
-  
+    await knex.schema.dropTableIfExists("shopping_list")
+    await knex.schema.dropTableIfExists("ingredients")
 };
