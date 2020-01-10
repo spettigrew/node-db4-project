09e3d2721f518bc40c05cc10eb357064a667c937
spettigrew <sara.pettigrew5@gmail.com>
2020-01-09 19:57:36 -0700

    Recipe migration setup

--- data/migrations/20200109113803_recipes.js
+++ data/migrations/20200109113803_recipes.js
@@ -1,10 +1,23 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("recipes", (table) => {
+      table.increments("id")
+      table.string("name").notNullable()
+  })
+
+  await knex.schema.createTable("shopping_list", (table) => {
+      table.integer("recipe_id")
+      .notNullable()
+      .references("id")
+      .inTable("recipes")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex) {     
+  await knex.schema.dropTableIfExists("shopping_list")
+  await knex.schema.dropTableIfExists("recipes")
 };
 
 
