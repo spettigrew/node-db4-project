ca42f5105ef1363f919bd5336be575daceb150a5
spettigrew <sara.pettigrew5@gmail.com>
2020-01-10 15:03:06 -0700

    Table columns added

--- data/migrations/20200109113915_shopping_list.js
+++ data/migrations/20200109113915_shopping_list.js
@@ -1,13 +1,18 @@
 
 exports.up = async function(knex) {
     await knex.schema.createTable("shopping_list", (table) => {
+        table.integer("recipe_id")
+            .notNullable()
+            .references("id")
+            .inTable("recipes")
         table.increments("id")
         table.integer("ingredient_id").notNullable()
         table.float("quantity")
         table.string("uom")
-        .notNullable()
-        .references("id")
-        .inTable("ingredients")
+            .notNullable()
+            .references("id")
+            .inTable("ingredients")
+        // table.primary(["recipe_id", "ingredient_id"]) //creates a primary key as columns
             .onDelete("CASCADE")
             .onUpdate("CASCADE")
     })
