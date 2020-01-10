8b647f5e8235ca48f31b2cb2f3221312aa0fce4e
spettigrew <sara.pettigrew5@gmail.com>
2020-01-09 20:05:58 -0700

    Shopping-list initiated

--- data/migrations/20200109113915_shopping_list.js
+++ data/migrations/20200109113915_shopping_list.js
@@ -1,8 +1,19 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable("shopping_list", (table) => {
+        table.increments("id")
+        table.integer("ingredient_id").notNullable()
+        table.float("quantity")
+        table.string("uom")
+        .notNullable()
+        .references("id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE")
+    })
 };
 
 exports.down = function(knex) {
-  
+    await knex.schema.dropTableIfExists("shopping_list")
 };
+
+// Do I need inTable("") for this join table?
